dabea9df1a9f0c52be2f0b0d95e08f677dbbccf6
yujintao529 <yujintao529@163.com>
2016-08-24 00:08:34 +0800

    1.增加广播权限的测试

---
+++ MyPractice/app/src/main/java/com/example/mypractice/broadcast/SendPermissionBro.java
@@ -0,0 +1,24 @@
+package com.example.mypractice.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.example.mypractice.Logger;
+
+/**
+ * <p>Copyright: Copyright (c) 2016</p>
+ * <p/>
+ * <p>Company: 浙江齐聚科技有限公司<a href="www.guagua.cn">www.guagua.cn</a></p>
+ *
+ * @author yujintao
+ * @version 1.0.0
+ * @description
+ * @modify
+ */
+public class SendPermissionBro extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Logger.d("onReceive "+intent.getAction());
+    }
+}
