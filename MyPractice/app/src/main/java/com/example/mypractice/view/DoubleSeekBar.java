407a1a016bdbe98a14af7bf05937bb0e6bc40396
yujintao <jintao@mafengwo.com>
2017-04-06 00:17:10 +0800

    修改默认的配置

--- MyPractice/app/src/main/java/com/example/mypractice/view/DoubleSeekBar.java
+++ MyPractice/app/src/main/java/com/example/mypractice/view/DoubleSeekBar.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
-import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
