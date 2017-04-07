2f1796b8592214568b1eb134738f269fc72f1d12
郁金涛 <jintao@mafengwo.com>
2017-04-07 11:50:47 +0800

    完成seekbar

--- MyPractice/app/src/main/java/com/example/mypractice/view/DoubleSeekBarAct.java
+++ MyPractice/app/src/main/java/com/example/mypractice/view/DoubleSeekBarAct.java
@@ -15,16 +15,32 @@ public class DoubleSeekBarAct extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_double_seek_bar);
-        DoubleSeekBar doubleSeekBar= (DoubleSeekBar) findViewById(R.id.seek_bar);
-        doubleSeekBar.setOnDotTextAdapter(new DoubleSeekBar.OnDotTextAdapter() {
+        MfwDoubleSeekBar doubleSeekBar = (MfwDoubleSeekBar) findViewById(R.id.seek_bar);
+        doubleSeekBar.setOnDotTextAdapter(new MfwDoubleSeekBar
+                .OnDotTextAdapter() {
             @Override
             public String content(int position, int value, int count) {
-                return position+"->"+value;
+                return position + "->" + value;
             }
         });
-        doubleSeekBar= (DoubleSeekBar) findViewById(R.id.seek_bar_2);
-        doubleSeekBar.setSeekBarMode(DoubleSeekBar.MODE_SINGLE);
-        doubleSeekBar.setCurrentValue(0,20);
+        doubleSeekBar.setOnScrollChangeListener(new MfwDoubleSeekBar.OnScrollChangeListener() {
+            @Override
+            public void onChanging(int startValue, int endValue) {
+                if (MfwCommon.DEBUG) {
+                    MfwLog.d(DoubleSeekBarAct.class.getSimpleName(), "onChanging  " + startValue + " " + endValue);
+                }
+            }
+
+            @Override
+            public void onChanged(int startValue, int endValue) {
+                if (MfwCommon.DEBUG) {
+                    MfwLog.d(DoubleSeekBarAct.class.getSimpleName(), "onChanged  " + startValue + " " + endValue);
+                }
+            }
+        });
+        doubleSeekBar = (MfwDoubleSeekBar) findViewById(R.id.seek_bar_2);
+        doubleSeekBar.setSeekBarMode(MfwDoubleSeekBar.MODE_SINGLE);
+        doubleSeekBar.setCurrentValue(0, 20);
 
     }
 
