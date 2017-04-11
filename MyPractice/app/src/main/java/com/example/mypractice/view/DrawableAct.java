50c2bc022b8a728df4270298fb2b86e73b1effac
yujintao <jintao@mafengwo.com>
2017-04-11 22:39:24 +0800

    增加基类

---
+++ MyPractice/app/src/main/java/com/example/mypractice/view/DrawableAct.java
@@ -0,0 +1,24 @@
+package com.example.mypractice.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * <p>Copyright: Copyright (c) 2016</p>
+ * <p>
+ * <p>Company: 浙江齐聚科技有限公司<a href="www.guagua.cn">www.guagua.cn</a></p>
+ *
+ * @author yujintao
+ * @version 1.0.0
+ * @description
+ * @modify
+ */
+
+public class DrawableAct extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
