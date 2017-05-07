17ac3f18851a6ee073b6c9729789eb7bbcc45170
郁金涛 <jintao@mafengwo.com>
2017-05-07 22:48:36 +0800

    完成layer-list

---
+++ MyPractice/app/src/main/java/com/example/mypractice/resource/drawable/LayerListDrawableAct.java
@@ -0,0 +1,22 @@
+package com.example.mypractice.resource.drawable;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.mypractice.R;
+
+/**
+ * Created by yujintao on 2017/5/7.
+ */
+
+public class LayerListDrawableAct extends AppCompatActivity{
+    public static final String TAG = LayerListDrawableAct.class.getSimpleName();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layerlist);
+    }
+
+}
