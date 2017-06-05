9318f518b7c312543dc74c9fbce183733021417b
yujintao <jintao@mafengwo.com>
2017-06-05 22:27:47 +0800

    删除progress的处理内容

--- MyPractice/app/src/main/java/com/example/mypractice/other/AdaliyaMeal.java
+++ MyPractice/app/src/main/java/com/example/mypractice/other/AdaliyaMeal.java
@@ -1,12 +1,10 @@
 package com.example.mypractice.other;
 
-import com.example.Factory;
-
 /**
  * Created by yujintao on 16/9/28.
  */
 
-@Factory(id = "adaliya",type = Meal.class)
+//@Factory(id = "adaliya",type = Meal.class)
 public class AdaliyaMeal implements Meal {
     private String price="22";
     @Override
--- MyPractice/app/src/main/java/com/example/mypractice/other/BaxiMeal.java
+++ MyPractice/app/src/main/java/com/example/mypractice/other/BaxiMeal.java
@@ -1,13 +1,11 @@
 package com.example.mypractice.other;
 
-import com.example.Factory;
-
 /**
  * Created by yujintao on 16/9/28.
  */
 
 
-@Factory(id = "baxi",type = Meal.class)
+//@Factory(id = "baxi",type = Meal.class)
 public class BaxiMeal implements Meal {
     private String price="11";
     @Override
