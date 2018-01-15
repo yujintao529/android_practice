97e7389ce8a96e311ee00605d13d88e11f3f1d07
郁金涛 <jintao@mafengwo.com>
2018-01-15 09:58:29 +0800

    增加kotlin支持

---
+++ MyPractice/app/src/main/java/com/example/mypractice/test/FrameLayoutTestAct.java
@@ -0,0 +1,20 @@
+package com.example.mypractice.test;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.mypractice.R;
+
+/**
+ * Created by yujintao on 2017/9/26.
+ */
+
+public class FrameLayoutTestAct extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_framelayouttest_layout);
+    }
+
+}
