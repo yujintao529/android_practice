82f9d6215a5e5f69da564a6a8e81f2b05e0ddc17
yujintao <jintao.yu@bitmain.com>
2018-08-22 17:51:59 +0800

    提交个文件

---
+++ MyPractice/app/src/main/java/com/example/mypractice/kotlin/LazyActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mypractice.kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class LazyActivity: AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
