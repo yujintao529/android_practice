92611e4e7490309998fadb8ad92b94169a13864c
yujintao529 <yujintao529@163.com>
2018-12-25 22:37:48 +0800

    temp

---
+++ MyPractice/app/src/main/java/com/demon/yu/glide/GlideTestAct.java
@@ -0,0 +1,19 @@
+package com.demon.yu.glide;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.example.mypractice.R;
+
+public class GlideTestAct extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glide);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Glide.with(this).load("").into(imageView);
+    }
+}
