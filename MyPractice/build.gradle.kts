55e7a1de19c8a22ef0b24b035f1c17540b258035
yujintao <yujintao.529@bytedance.com>
2021-11-18 20:40:51 +0800

    完成kotlin化配置
    Signed-off-by: yujintao <yujintao.529@bytedance.com>

---
+++ MyPractice/build.gradle.kts
@@ -0,0 +1,49 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+//这个是整个项目的构建文件，他的作用阀是通过setttings.gradle来控制的。
+buildscript {
+    repositories {
+        jcenter()
+        mavenCentral()
+        flatDir { dirs("libs") }
+        google()
+    }
+    dependencies {
+        classpath(Deps.androidToolsBuildGradle)
+        classpath(Deps.kotlinGradlePlugin)
+    }
+}
+allprojects {
+    repositories {
+        maven {
+            setUrl("https://oss.sonatype.org/content/repositories/snapshots/")
+        }
+        google()
+        jcenter()
+        mavenCentral()
+    }
+}
+//buildscript {
+//    repositories {
+//        jcenter()
+//        mavenCentral()
+//        flatDir {
+//            dirs 'libs'
+//        }
+//        google()
+//    }
+//    dependencies {
+//        classpath Deps.androidToolsBuildGradle
+//                classpath Deps.kotlinGradlePlugin
+//    }
+//}
+//
+////定义依赖仓库
+//allprojects {
+//    repositories {
+//        maven { url "https://oss.sonatype.org/content/repositories/snapshots/" }
+//        google()
+//        jcenter()
+//        mavenCentral()
+//    }
+//}
+
