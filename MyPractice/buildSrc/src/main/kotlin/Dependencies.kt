e0a752fe2ddec518753eddec302ab71e83a99b64
yujintao <yujintao.529@bytedance.com>
2022-11-14 14:01:10 +0800

    增加fitSystem

--- MyPractice/buildSrc/src/main/kotlin/Dependencies.kt
+++ MyPractice/buildSrc/src/main/kotlin/Dependencies.kt
@@ -137,7 +137,7 @@ object Deps {
 
 object Versions {
     const val compileSdkVersion = 29
-    const val minSdkVersion = 23
+    const val minSdkVersion = 21
     const val targetSdkVersion = 29
     const val buildToolsVersion = "29.0.2"
 }
